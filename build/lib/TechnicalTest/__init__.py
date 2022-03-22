7dd76196d02f0faaa306f5e85d1f260e96ddef62
Marina Lacambra <marina.lacambra@gmail.com>
2022-03-22 20:22:34 +0100

    Requirements.txt fixed to be installed with pip

--- build/lib/TechnicalTest/__init__.py
+++ build/lib/TechnicalTest/__init__.py
@@ -4,5 +4,5 @@ TechnicalTest.
 Marina Lacambra's Technical Test.
 """
 
-__version__ = "0.2.5"
+__version__ = "0.3.3"
 __author__ = 'Marina Lacambra'
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='TechnicalTest',
-    version='0.2.5',
+    version='0.3.3',
     author='Marina Lacambra',
     author_email='marina.lacambra@gmail.com',
     packages=['TechnicalTest'],
@@ -11,5 +11,9 @@ setup(
     description='Technical Test',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    install_requires=["preprocessor", "nltk", "pandas", "sklearn"]
+    install_requires=[
+"tweet-preprocessor==0.6.0",
+"pandas==1.3.4",
+"nltk==3.6.5",
+"sklearn"]
 )
